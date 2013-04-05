e0833a1be1c8180ae3dbad8397e23b7c79173517
kenneth koontz <kenneth.koontz@gmail.com>
2013-04-04 21:25:56 -0700

    added client side angular app

--- public/javascripts/referral.js
+++ public/javascripts/referral.js
@@ -1,9 +1,40 @@
 var referrals = angular.module('referrals', [])
 
 var ManagerCtrl = function($scope) {
+  $scope.newReferralTitle = ''
   // TODO: When server data model is made, remove this and user server
   $scope.referrals = [ { title: 'poseidon', clicks: 0 }
                      , { title: 'ares', clicks: 0 }
                      , { title: 'zeus', clicks: 0 }
                      ]
+
+  $scope.add = function() {
+    var referral = { title: $scope.newReferralTitle
+                   , clicks: 0
+                   }
+
+    $scope.referrals.push(referral)
+    $scope.newReferralTitle = ''
+  }
+
+  $scope.setCurrentLink = function(r) {
+    $scope.currentLink = r
+    $scope.newTitle = $scope.currentLink.title
+  }
+
+  $scope.delete = function() {
+    for (var i in $scope.referrals) {
+      if ($scope.referrals[i].title === $scope.currentLink.title) {
+        return $scope.referrals.splice(i, 1)
+      }
+    }
+  }
+
+  $scope.edit = function() {
+    for (var i in $scope.referrals) {
+      if ($scope.referrals[i].title === $scope.currentLink.title) {
+        return $scope.referrals[i].title = $scope.newTitle
+      }
+    }
+  }
 }
